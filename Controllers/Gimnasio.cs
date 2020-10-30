feb0093aab8a36905142277d432aa19081ee887f
AndresBetancourt-Dev <contacto.betancourth@gmail.com>
2020-10-30 00:21:23 -0500

    Added Templatess for Gym, Center and Health Views, polished the Home and the Courses Index

---
+++ Controllers/Gimnasio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Wellness_USC.Controllers
+{
+    public class Gimnasio : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
